e62ebf7faea54b6df15696efd8b66d88d618bbcc
liuheyong <17682347237@163.com>
2019-06-12 13:02:06 +0800

    commit

---
+++ src/main/java/com/cloud2/discover/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cloud2.discover;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ZCloud2DiscoverApplication.class);
+    }
+
+}
---
+++ src/main/java/com/cloud2/discover/ZCloud2DiscoverApplication.java
@@ -0,0 +1,16 @@
+package com.cloud2.discover;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ZCloud2DiscoverApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ZCloud2DiscoverApplication.class).web(true).run(args);
+    }
+
+
+}
